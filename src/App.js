282d3a24b06fec1894d86793b35c20449725fe72
Faddal Ibrahim <41176161+faddalibrahim@users.noreply.github.com>
2021-03-17 02:06:02 +0000

    home page done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/home/Home.js";
+import About from "./pages/about/About.js";
+import Comment from "./pages/comment/Comment.js";
+import Error404 from "./pages/error/Error404.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/comment" component={Comment} />
+          <Route path="*" component={Error404} />
+        </Switch>
+      </Router>
     </div>
   );
 }
